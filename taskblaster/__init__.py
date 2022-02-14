7cf540cc19959ec8884730768f043fe8f94cdf29
Owain Kenway <o.kenway@ucl.ac.uk>
2022-02-14 15:48:11 +0000

    Hopefully nice output.

--- taskblaster/__init__.py
+++ taskblaster/__init__.py
@@ -11,15 +11,30 @@ def RunTasks(tasks):
     # work out which tasks are mine.
     stencil = []
     i = rank
+
+    if (rank == 0):
+        print("Decomposition:\n")
+
+    comm.Barrier()
+
     while (i < len(tasks)):
         stencil.append(i)
+        print(rank, ": ", tasks[i])
         i += size
+
+    comm.Barrier()
+    if (rank == 0):
+        print("\nRunning tasks:\n")    
+    comm.Barrier()
 
     for j in stencil:
         print(rank, ": ", tasks[j])
         status = ge.run(tasks[j])
-        print(rank, ": ", "Return code: ", status.returncode)
-        print(rank, ": ", "Stdout: ", status.stdout)
-        print(rank, ": ", "Stderr: ", status.stderr)
-
+        print(rank, ": ", tasks[j], "\n",
+              rank, ": ", "Return code: ", status.returncode, "\n",
+              rank, ": ", "Stdout: ", status.stdout, "\n",
+              rank, ": ", "Stderr: ", status.stderr)
     comm.Barrier()
+
+    if (rank == 0):
+        print("\nFinished\n")    
